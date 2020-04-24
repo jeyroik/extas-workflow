92e146aeee1eca143f9dfc999fd7b47aa5e96cb6
jeyroik <jeyroik@gmail.com>
2020-04-24 16:00:02 +0300

    tests

--- tests/workflows/schemas/WorkflowSchemaTest.php
+++ tests/workflows/schemas/WorkflowSchemaTest.php
@@ -81,7 +81,7 @@ class WorkflowSchemaTest extends TestCase
         $this->entityTemplateRepo->delete([WorkflowEntityTemplate::FIELD__NAME => 'test']);
         $this->transitionDispatcherRepo->delete([TransitionDispatcher::FIELD__NAME => 'test']);
         $this->transitionDispatcherTemplateRepo->delete([TDT::FIELD__NAME => 'test']);
-        $this->transitionRepo->delete([WorkflowTransition::FIELD__NAME => 'test']);
+        $this->transitionRepo->delete([WorkflowTransition::FIELD__NAME => ['test', 'test2']]);
     }
 
     /**
